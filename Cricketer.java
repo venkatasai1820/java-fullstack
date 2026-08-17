bab236de55adb062a19631698d677f9afdb49df5
venkatasai1820 <venkatasaithirumalagiri@gmail.com>
2026-08-17 20:05:02 +0530

    test commit

---
+++ Cricketer.java
@@ -0,0 +1,66 @@
+
+
+
+public class Cricketer {
+		
+		static int CountryId;
+		static String CountryName;
+		
+		String PlayerName;
+		int JersseyNumber;
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			CountryId = 91;
+			CountryName = "india";
+			
+			Cricketer s2 = new Cricketer();
+			s2.PlayerName = "Dhoni";
+			s2.JersseyNumber = 07;
+			System.out.println(CountryId);
+			System.out.println(CountryName);
+			System.out.println(s2.PlayerName);
+			System.out.println(s2.JersseyNumber);
+			
+			
+			CountryId = 92;
+			CountryName = "india";
+			
+			Cricketer s3= new Cricketer();
+			s3.PlayerName = "virat";
+			s3.JersseyNumber = 18;
+			System.out.println(CountryId);
+			System.out.println(CountryName);
+			System.out.println(s3.PlayerName);
+			System.out.println(s3.JersseyNumber);
+			
+			CountryId = 92;
+			CountryName = "india";
+			
+			Cricketer kl= new Cricketer();
+			kl.PlayerName = "k lokesh rahul";
+			kl.JersseyNumber = 101;
+			System.out.println(CountryId);
+			System.out.println(CountryName);
+			System.out.println(kl.PlayerName);
+			System.out.println(kl.JersseyNumber);
+			
+			CountryId = 92;
+			CountryName = "india";
+			
+			Cricketer vaibhav= new Cricketer();
+			vaibhav.PlayerName = "vaibhav suryavanshi";
+			vaibhav.JersseyNumber = 03;
+			System.out.println(CountryId);
+			System.out.println(CountryName);
+			System.out.println(vaibhav.PlayerName);
+			System.out.println(vaibhav.JersseyNumber);
+
+			
+		}
+
+
+
+}
