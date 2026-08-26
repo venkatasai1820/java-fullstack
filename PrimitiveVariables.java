243c82561ce03dd99a2c6f34035ab511ac975438
venkat1820 <venkatasaithirumalagiri@gmail.com>
2026-08-26 10:06:38 +0530

    Primitive varibles file

---
+++ PrimitiveVariables.java
@@ -0,0 +1,28 @@
+
+
+
+
+public class PrimitiveVariables {
+	
+	
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		Integer b = 100;
+		
+		b=a;
+		
+		Integer i = new Integer("100");
+		
+		int c=i;
+	   System.out.println(b = a);
+	   System.out.println(c);
+		
+	
+		
+		
+
+	}
+
+}
